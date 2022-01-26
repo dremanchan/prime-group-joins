d3648912b246712fb8aca79c729e37d5a7f9cc93
Andre Manchanthasouk <dremanchan@gmail.com>
2022-01-26 00:08:58 -0600

    p5 done

--- joins-solution.sql
+++ joins-solution.sql
@@ -17,8 +17,37 @@ JOIN "products"
 
 -- 3. Which warehouses have cheetos?
 
-SELECT *
+SELECT 
+    warehouse.warehouse,
+    products.description
 FROM "warehouse"
-JOIN "warehouse_product"
-    ON "warehouse_id" = "warehouse".id;
+JOIN "warehouse_product" ON "warehouse_product"."warehouse_id" = warehouse.id
+JOIN products ON warehouse_product.product_id = products.id
+WHERE products.description = 'cheetos';
+
+-- 4. Which warehouses have diet pepsi?
+
+SELECT 
+    warehouse.warehouse,
+    products.description
+FROM "warehouse"
+JOIN "warehouse_product" ON "warehouse_product"."warehouse_id" = warehouse.id
+JOIN products ON warehouse_product.product_id = products.id
+WHERE products.description = 'diet pepsi';
+
+-- 5. Get the number of orders for each customer. 
+-- NOTE: It is OK if those without orders are not included in results.
+
+SELECT
+    count(orders.id) AS order_count,
+    CONCAT (customers.first_name, ' ', customers.last_name) AS name
+FROM customers
+JOIN addresses ON customers.id = addresses.customer_id
+JOIN orders ON orders.address_id = addresses.id
+Group by name;
+
+
+
+
+
 
